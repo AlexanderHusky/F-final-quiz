5d56dfbc730b22c818b38dfea64e7989ad71a014
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-10-30 10:56:28 +0800

    [mengqiu] add feedback

--- src/App/assets/components/Groups.js
+++ src/App/assets/components/Groups.js
@@ -7,22 +7,23 @@ class Groups extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // TODO GTB-知识点: - students和list1-6这个数据结构,可以看出来你缺乏数据结构方面基础较差。建议补充一下这方面的知识。
       students: '',
       // eslint-disable-next-line react/no-unused-state
-      list1:[],
+      list1: [],
       // eslint-disable-next-line react/no-unused-state
-      list2:[],
+      list2: [],
       // eslint-disable-next-line react/no-unused-state
-      list3:[],
+      list3: [],
       // eslint-disable-next-line react/no-unused-state
-      list4:[],
+      list4: [],
       // eslint-disable-next-line react/no-unused-state
-      list5:[],
+      list5: [],
       // eslint-disable-next-line react/no-unused-state
-      list6:[]
+      list6: []
     };
   }
-
+  // TODO GTB-知识点: - 生命周期函数要写到class Groups里边去
   async componentDidMount() {
     const api = 'http://localhost:8080/trainee';
     const students = await axios.get(api).then((stu) => stu.data);
@@ -30,5 +31,5 @@ class Groups extends React.Component {
   }
 
 
-
+// TODO GTB-综合: - 语法错误
 export default Groups;
